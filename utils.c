67396eac58c87b565e2250b54682a307bb2b51b7
maxwellcudjoe <58773724+maxwellcudjoe@users.noreply.github.com>
2023-07-24 04:42:56 +0000

    Update utils.c

--- utils.c
+++ utils.c
@@ -15,7 +15,7 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - by Maxwell and Kagiso Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
@@ -38,7 +38,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
+ * is_digit -by Maxwell & Kagiso Verifies if a char is a digit
  * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
